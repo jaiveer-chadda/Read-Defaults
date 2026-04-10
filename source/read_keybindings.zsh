34976bfb554e0cf3575db2953a4fc91e6f2ed5af
JV <jaiveer.chadda@gmail.com>
2026-04-11 00:20:10 +0100

    Realised that the is_reading var was irrelevant, so removed it. Also generally cleaned up the code, added comments everywhere, and moved some string literals into variables

--- source/read_keybindings.zsh
+++ source/read_keybindings.zsh
@@ -1,20 +1,26 @@
 #!/usr/bin/env zsh
 
+# if __name__ != "__main__": quit()
 if [[ $ZSH_EVAL_CONTEXT != 'toplevel' ]] return 1
-
 
 () {
 
-  local -ri 10 _from_cache=1
+  local -ri 2 _load_from_cache=1
   local -r _cache_file='../cache/latest.txt'
   local -r _defaults_key='NSUserKeyEquivalents'
 
   local -ri 10 delim=$RANDOM
   local -r _sep=' -> '
 
+  local -r _list_start_pattern="^Found 1 keys in domain '([^']+)'"
+  local -r _useless_line_pattern='^\s+"'
+  local -r _list_end_pattern='}'
+
+  # ———————————————————————————————————————————————————————————————————————— #
+
   local defaults_raw
 
-  if (( _from_cache )) {
+  if (( _load_from_cache )) {
     defaults_raw="$( cat "$_cache_file" )"
   } else {
     defaults_raw="$( defaults find "$_defaults_key" | tee "$_cache_file" )"
@@ -27,35 +33,31 @@ if [[ $ZSH_EVAL_CONTEXT != 'toplevel' ]] return 1
   # ———————————————————————————————————————————————————————————————————————— #
 
   local -A defaults_parsed
-  
-  local -i 10 is_reading=0
-  local line bundle_id inner_lines
 
   # autoload -Uz regexp-replace
-  setopt rematch_pcre
-  setopt extended_glob
+  setopt rematch_pcre   # for ${match[1]}
+  setopt extended_glob  # for ${line/# ##...
+
+  local line bundle_id inner_lines
 
   for line in "${(@f)defaults_raw}"; {
-    
+
     # start of key list
-    if (( ! is_reading )) \
-     && [[ "$line" =~ "^Found 1 keys in domain '([^']+)'" ]] {
+    if [[ "$line" =~ "${~_list_start_pattern}" ]] {
       bundle_id="${match[1]}"
       inner_lines=  # reset the inner lines
-      is_reading=1
       continue
     }
 
     # end of list
-    if (( is_reading )) && [[ "$line" == '}' ]] {
+    if [[ "$line" == "$_list_end_pattern" ]] {
       # push the contents into the output array
       defaults_parsed[$bundle_id]="$inner_lines"
-      is_reading=0
       continue
     }
 
     # if it doesn't start with a quote, it's irrelevant to us
-    if (( is_reading )) && [[ ! "$line" =~ '^\s+"' ]] continue
+    if [[ ! "$line" =~ "${~_useless_line_pattern}" ]] continue
 
     # everything else that hasn't been matched before is a keybinding entry
     line="${line/# ##\"}"        # strip the leading spaces and quote
